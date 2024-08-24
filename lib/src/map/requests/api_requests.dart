99a06d0e90cf858c88616077c3099209f52307b8
Pranim Singh Thakuri <pranimsingh7@gmail.com>
2024-08-24 09:02:39 +0545

    Initial commit

--- lib/src/map/requests/api_requests.dart
+++ lib/src/map/requests/api_requests.dart
@@ -7,15 +7,17 @@ import '../navigation/model/direction_route_response.dart';
 import '../navigation/model/enums.dart';
 import 'package:http/http.dart' as http;
 
-
-
-class ApiRequest{
+class ApiRequest {
   // String baseUrl = 'https://api.mapbox.com/directions/v5/mapbox';
   // String navType = 'cycling';
 
 //
-  Future getDrivingRouteUsingMapbox({required LatLng source, required LatLng destination, required mapBoxAccessToken}) async {
-    String url = 'https://api.mapbox.com/directions/v5/mapbox/driving/${source.longitude},${source.latitude};${destination.longitude},${destination.latitude}?alternatives=true&continue_straight=true&geometries=geojson&language=en&overview=full&steps=true&access_token=$mapBoxAccessToken';
+  Future<Object?> getDrivingRouteUsingMapbox(
+      {required LatLng source,
+      required LatLng destination,
+      required mapBoxAccessToken}) async {
+    String url =
+        'https://api.mapbox.com/directions/v5/mapbox/driving/${source.longitude},${source.latitude};${destination.longitude},${destination.latitude}?alternatives=true&continue_straight=true&geometries=geojson&language=en&overview=full&steps=true&access_token=$mapBoxAccessToken';
 
     debugPrint(url);
     try {
@@ -23,22 +25,25 @@ class ApiRequest{
       return json.decode(httpResponse.body);
     } catch (e) {
       debugPrint(e.toString());
+      return null;
     }
   }
 
-  Future getDrivingRouteUsingGraphHooper(
+  Future<DirectionRouteResponse?> getDrivingRouteUsingGraphHooper(
       {String? customBaseUrl,
-        required LatLng source,
+      required LatLng source,
       required LatLng destination,
       required NavigationProfile? navigationType,
       required String graphHooperApiKey}) async {
-
-    String routeUrl = customBaseUrl!.isNotEmpty ? customBaseUrl :'https://graphhopper.com/api/1';
+    String routeUrl = customBaseUrl!.isNotEmpty
+        ? customBaseUrl
+        : 'https://graphhopper.com/api/1';
 
     String startPoint = '${source.latitude}%2C${source.longitude}';
     String endPoint = '${destination.latitude}%2C${destination.longitude}';
 
-    String url = '$routeUrl/route?point=$startPoint&point=$endPoint&profile=${navigationType!.name}&points_encoded=false&key=$graphHooperApiKey';
+    String url =
+        '$routeUrl/route?point=$startPoint&point=$endPoint&profile=${navigationType!.name}&points_encoded=false&key=$graphHooperApiKey';
 
     debugPrint(url);
     try {
@@ -46,8 +51,7 @@ class ApiRequest{
       return DirectionRouteResponse.fromJson(json.decode(httpResponse.body));
     } catch (e) {
       debugPrint(e.toString());
+      return null;
     }
   }
-
 }
-
